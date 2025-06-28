e519d93615518d6727852b59fd17b42c662d93a2
Chris Idema <48433711+ChrisIdema@users.noreply.github.com>
2025-06-28 12:31:30 +0200

    intermediate commit, allows command line argument of seed

--- snowflake.py
+++ snowflake.py
@@ -1,16 +1,18 @@
 from math import atan2, degrees, radians, sin, cos
 import random  # random, choice, randint
+import time
+import sys
 
+
+# render preferences:
 grid_width = 79 # has to be odd
-font_height_to_width = 32/14 # set this to your font ratio
-
+font_height_to_width = 32/14 # set this to your font ratio. (in vscode I used 32/14, but on "https://www.online-python.com/" I used 20/7)
 grid_height = int(((grid_width / font_height_to_width)//2)*2+1)
 max_length_sq = 0.9**2
 
 
-
+# creates a tree with random branch posititions, angles and lengths, doesn't create mirrored branches
 def create_half_tree(level, number_of_levels):
-
     node = {"connection": 0,                
     "l" : 0,
     "w": 0,
@@ -51,12 +53,14 @@ def create_half_tree(level, number_of_levels):
     return node
 
 
+# mirrors a node since all branches are symmetrical
 def mirror_node(node):
     copy = node.copy()
     copy["angle"] *= -1
     return copy
 
 
+# mirrors all nodes in a tree, starting at leaf nodes
 def mirror_tree(node):
     for child in node["children"]:
         mirror_tree(child)
@@ -65,6 +69,7 @@ def mirror_tree(node):
     node["children"] += mirror_children
 
 
+# extracts all individual nodes from a tree, convertes relative dimensions into absoulte dimensions
 def get_nodes(root, nodes=[],l=1,w=1, angle=0, origin=[0,0]):
     node = root.copy()
     node["children"] = []
@@ -84,6 +89,7 @@ def get_nodes(root, nodes=[],l=1,w=1, angle=0, origin=[0,0]):
     return nodes
 
 
+# rotates a point around origin, used to see if a point intersects with a rotated square
 def rotate(origin, point, angle):    
     ox, oy = origin
     px, py = point
@@ -93,6 +99,7 @@ def rotate(origin, point, angle):
     return [x2,y2]
 
 
+# checks if a point intersects with a rotated square
 def is_in_node(node, point):
     # rotate point around origin in opposite direction
     x,y = rotate(node["origin"],point,-node["angle"])
@@ -101,52 +108,68 @@ def is_in_node(node, point):
     return  x >= node["origin"][0] - node["w"] and x <= node["origin"][0] + node["w"] and \
             y <= node["origin"][1] and y >= node["origin"][1] - node["l"]
 
+# create a full random tree using a seed and returns all nodes
+def create_tree(seed=None):
+    random.seed(seed)
+    tree = create_half_tree(0, random.randint(1,3))
+    mirror_tree(tree)
+    nodes = get_nodes(tree)
+    return nodes
 
-#random.seed(4)
-test_tree = create_half_tree(0, random.randint(1,3))
-mirror_tree(test_tree)
-nodes = get_nodes(test_tree)
-# print(nodes)
 
-flake = ""
-for y in range(grid_height):
-    line = list(" "*grid_width+"\n")
-    for x in range(grid_width):
-        # get coordinates relative to center:
-        dx = (x - grid_width//2) / (grid_width//2)
-        dy = (y - grid_height//2) / (grid_height//2)
-        length_sq = dx**2 + dy**2 # length squared so no need to calculate square root
+# renders the nodes in ascii, an svg function is under development
+def render_flake_ascii(nodes):
+    flake = ""
+    for y in range(grid_height):
+        line = list(" "*grid_width+"\n")
+        for x in range(grid_width):
+            # get coordinates relative to center:
+            dx = (x - grid_width//2) / (grid_width//2)
+            dy = (y - grid_height//2) / (grid_height//2)
+            length_sq = dx**2 + dy**2 # length squared so no need to calculate square root
 
-        angle_degrees = degrees(atan2(dy,dx)) + 90
-        if angle_degrees > 180:
-            angle_degrees -= 360
+            angle_degrees = degrees(atan2(dy,dx)) + 90
+            if angle_degrees > 180:
+                angle_degrees -= 360
 
-        if length_sq == 0:
-            line[x] = '~'
-        elif length_sq > max_length_sq:
-            pass 
-        else:
-            segment_index = int(((angle_degrees + 30 + 360) %360)//60)
-            octant_index = round(((angle_degrees + 360) %360)/45)%8
-            octant_array = "|/=\\|/=\\"
-            octant_char = octant_array[octant_index] 
+            if length_sq == 0:
+                line[x] = '~'
+            elif length_sq > max_length_sq:
+                pass 
+            else:
+                segment_index = int(((angle_degrees + 30 + 360) %360)//60)
+                octant_index = round(((angle_degrees + 360) %360)/45)%8
+                octant_array = "|/=\\|/=\\"
+                octant_char = octant_array[octant_index] 
 
-            hex_array = "|//z=\\\\N|/7z=\\\\N"
+                hex_array = "|//z=\\\\N|/7z=\\\\N"
 
-            dx2,dy2 = rotate([0,0],[dx,dy],segment_index*-60)
-            for node in nodes:                
-                in_node = is_in_node(node,[dx2,dy2])
-                if in_node:
-                    # octant_index = round(((node["angle"]+segment_index*60 + 360) %360)/45)%8
-                    # octant_char = octant_array[octant_index]                        
-                    # line[x] = octant_char
+                dx2,dy2 = rotate([0,0],[dx,dy],segment_index*-60)
+                for node in nodes:                
+                    in_node = is_in_node(node,[dx2,dy2])
+                    if in_node:
+                        # octant_index = round(((node["angle"]+segment_index*60 + 360) %360)/45)%8
+                        # octant_char = octant_array[octant_index]                        
+                        # line[x] = octant_char
 
-                    hex_index = round(((node["angle"]+segment_index*60 + 360) %360)/22.5)%16
-                    hex_char = hex_array[hex_index]                        
-                    line[x] = hex_char
+                        hex_index = round(((node["angle"]+segment_index*60 + 360) %360)/22.5)%16
+                        hex_char = hex_array[hex_index]                        
+                        line[x] = hex_char
 
-                    break
+                        break
 
-    flake += ''.join(line)
+        flake += ''.join(line)
+    return flake
 
-print(flake)
+
+if __name__ == "__main__":
+    seed = None
+    if len(sys.argv) > 1:
+        seed = int(sys.argv[1])
+   
+    if not seed:
+        seed = int(time.time()*10)
+    print(f"seed: {seed}")
+    nodes = create_tree(seed)
+    ascii_flake = render_flake_ascii(nodes)
+    print(ascii_flake)
